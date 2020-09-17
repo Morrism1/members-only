9fea419e79d1f62a12fb2767595ebbce96bd55a5
Morrism1 <morrismontana0@gmail.com>
2020-09-17 17:07:16 +0200

    Add title field to post

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_15_154712) do
+ActiveRecord::Schema.define(version: 2020_09_17_145555) do
 
   create_table "posts", force: :cascade do |t|
     t.text "post"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "user_id", null: false
+    t.string "title"
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
