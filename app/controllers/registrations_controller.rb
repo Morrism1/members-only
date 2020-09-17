f193fbeca2c10944beb3414d3abe58d46114ba00
Morrism1 <morrismontana0@gmail.com>
2020-09-17 20:20:01 +0200

    Fix rubocop errors

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,8 +1,7 @@
 class RegistrationsController < Devise::RegistrationsController
-
   private
-  
+
   def sign_up_params
-    params.require(:user).permit(:name, :username, :email, :password, :password_confirmation )
+    params.require(:user).permit(:name, :username, :email, :password, :password_confirmation)
   end
 end
