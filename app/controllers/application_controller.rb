e2f581f3cd8433ce4b06bb24479f7773d98e8924
Morrism1 <morrismontana0@gmail.com>
2020-09-21 14:32:09 +0200

    Fix issues with logged_in and rubocop errors

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,17 +1,11 @@
 class ApplicationController < ActionController::Base
-    helper_method :logged_in?, :current_user
+  helper_method :logged_in?, :current_user
 
-    def current_user
-        if session[:user_id]
-          @user = User.find(session[:user_id])
-        end
-    end
+  def logged_in?
+    !current_user.nil?
+  end
 
-    def logged_in?
-      !!current_user
-    end
-
-    def authorized
-      redirect_to new_user_session_path unless logged_in?
-    end
+  def authorized
+    redirect_to new_user_session_path unless logged_in?
+  end
 end
