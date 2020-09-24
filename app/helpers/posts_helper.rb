34238ad68b80f669e659383d625df1ef544434e5
Morrism1 <morrismontana0@gmail.com>
2020-09-24 10:38:11 +0200

    Fix rubocop errors

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,13 +1,14 @@
 module PostsHelper
   def logged_in_links(post)
     return unless current_user &.== post.user
+
     content_tag :div do
       (link_to 'Show', post, class: 'card-link') +
         (link_to 'Edit', edit_post_path(post), class: 'card-link') +
         (link_to 'Destroy', post, method: :delete, data: { confirm: 'Are you sure?' }, class: 'card-link')
     end
   end
-  
+
   def post_author(post)
     content_tag(:h6, 'Written by: ' + post.user.name, class: ['card-subtitle mb-2 text-muted']) if logged_in?
   end
